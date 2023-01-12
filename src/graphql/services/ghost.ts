fda638d166cbd7bf0dfd50bc26a962f7ee849151
Jman4190 <mannelly.john@gmail.com>
2023-01-11 21:26:01 -0500

    Update ghost version
    Added 5.0

--- src/graphql/services/ghost.ts
+++ src/graphql/services/ghost.ts
@@ -3,5 +3,5 @@ import GhostContentAPI from '@tryghost/content-api'
 export const ghost = new GhostContentAPI({
   url: 'https://jman-dot-com.ghost.io',
   key: process.env.GHOST_API_KEY,
-  version: 'v5',
+  version: 'v5.0',
 })

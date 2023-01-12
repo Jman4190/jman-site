abd4328e0d1d20f02bab67ea142e109f368fd9d5
Jman4190 <mannelly.john@gmail.com>
2023-01-11 21:32:31 -0500

    Trying v4
    Updating version

--- src/graphql/services/ghost.ts
+++ src/graphql/services/ghost.ts
@@ -3,5 +3,5 @@ import GhostContentAPI from '@tryghost/content-api'
 export const ghost = new GhostContentAPI({
   url: 'https://jman-dot-com.ghost.io',
   key: process.env.GHOST_API_KEY,
-  version: 'v5.0',
+  version: 'v4',
 })
